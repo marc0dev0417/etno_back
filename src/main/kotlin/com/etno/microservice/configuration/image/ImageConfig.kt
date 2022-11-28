b7f49bd34a33e4e062e66d426901230998536646
marcobenegas <marcobenegas@ecomputer.es>
2022-11-28 18:12:31 +0100

    EB/10: Uploaded of images winded up and user, event, image test created to check if endpoint can operate very good.

---
+++ src/main/kotlin/com/etno/microservice/configuration/image/ImageConfig.kt
@@ -0,0 +1,14 @@
+package com.etno.microservice.configuration.image
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class ImageConfig: WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        super.addResourceHandlers(registry)
+
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///${System.getProperty("user.dir")}/src/main/resources/images/")
+    }
+}
