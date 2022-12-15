9d9a679463e4f568b10e2f0d5e9b8549ee14c27c
marcobenegas <marcobenegas@ecomputer.es>
2022-12-15 08:54:38 +0100

    EB/17: Added new model to section and subscription with their endpoints

--- src/main/kotlin/com/etno/microservice/repository/SectionSubscribeRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.etno.microservice.repository
-
-import com.etno.microservice.model.SectionSubscribe
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface SectionSubscribeRepository: JpaRepository<SectionSubscribe, UUID> {
-
-}
--- src/main/kotlin/com/etno/microservice/service/SectionSubscribeInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.etno.microservice.service
-
-import com.etno.microservice.model.dto.SectionSubscribeDTO
-
-interface SectionSubscribeInterface {
-   fun getSectionSubscribe(): List<SectionSubscribeDTO>?
-}
